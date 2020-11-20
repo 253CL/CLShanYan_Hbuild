5e4902b1b42ec4d8374667aa339df3c4fe5d82c2
fugang <fug@253.com>
2020-11-20 11:53:14 +0800

    android升级到2323版本，ios升级到2335版本

--- CLShanYan_Hbuild_demo/nativeplugins/CLSDK-ShanYanSDKModule/ios/CL_ShanYanSDK.framework/Headers/CLShanYanSDKManager.h
+++ CLShanYan_Hbuild_demo/nativeplugins/CLSDK-ShanYanSDKModule/ios/CL_ShanYanSDK.framework/Headers/CLShanYanSDKManager.h
@@ -20,20 +20,56 @@
  * @param index       协议位置--0:运营商协议--1:用户协议一--2:用户协议二
  * @param telecom     当前运营商类型
 */
--(void)clShanYanSDKManagerWebPrivacyClicked:(NSString *_Nonnull)privacyName privacyIndex:(NSInteger)index currentTelecom:(NSString *_Nullable)telecom;
+-(void)clShanYanSDKManagerWebPrivacyClicked:(NSString *_Nonnull)privacyName privacyIndex:(NSInteger)index currentTelecom:(NSString *_Nullable)telecom DEPRECATED_MSG_ATTRIBUTE("Method deprecated. Use `clShanYanActionListner:code:message:`");
 
 /**
- * 授权页面将要显示的回调
- * ViewDidLoad After
+ * 授权页面已经显示的回调
+ * ViewDidAppear
  * @param telecom     当前运营商类型
  */
--(void)clShanYanSDKManagerAuthPageAfterViewDidLoad:(UIView *_Nonnull)authPageView currentTelecom:(NSString *_Nullable)telecom;
+-(void)clShanYanSDKManagerAuthPageAfterViewDidLoad:(UIView *_Nonnull)authPageView currentTelecom:(NSString *_Nullable)telecom ;
 
+/**
+ * 授权页面将要显示的回调 ViewDidLoad即将全部执行完毕的最后时机
+ * ViewDidLoad  did complete
+ * @param telecom     当前运营商类型
+ */
+-(void)clShanYanSDKManagerAuthPageCompleteViewDidLoad:(UIViewController *_Nonnull)authPageVC currentTelecom:(NSString *_Nullable)telecom object:(NSObject*_Nullable)object userInfo:(NSDictionary*_Nullable)userInfo;
+/**
+ * 授权页面将要显示的回调
+ * ViewWillAppear
+ * @param telecom     当前运营商类型
+ */
+-(void)clShanYanSDKManagerAuthPageCompleteViewWillAppear:(UIViewController *_Nonnull)authPageVC currentTelecom:(NSString *_Nullable)telecom object:(NSObject*_Nullable)object userInfo:(NSDictionary*_Nullable)userInfo;
+/**
+ * 授权页vc alloc init
+ * init，注：此时authPageVC.navigationController为nil
+ * @param telecom     当前运营商类型
+ */
+-(void)clShanYanSDKManagerAuthPageCompleteInit:(UIViewController *_Nonnull)authPageVC currentTelecom:(NSString *_Nullable)telecom object:(NSObject*_Nullable)object userInfo:(NSDictionary*_Nullable)userInfo;
+/**
+ * 授权页vc 将要被present
+ * 将要调用[uiconfigure.viewcontroller  present:authPageVC animation:completion:]
+ * @param telecom     当前运营商类型
+ */
+-(void)clShanYanSDKManagerAuthPageWillPresent:(UIViewController *_Nonnull)authPageVC currentTelecom:(NSString *_Nullable)telecom object:(NSObject*_Nullable)object userInfo:(NSDictionary*_Nullable)userInfo;
+
+/**
+ * 统一事件监听方法
+ * type：事件类型（1，2，3）
+ * 1：隐私协议点击
+ * - 同-clShanYanSDKManagerWebPrivacyClicked:privacyIndex:currentTelecom
+ * code：0,1,2,3（协议页序号），message：协议名_当前运营商类型
+ * 2：协议勾选框点击
+ * code：0,1（0为未选中，1为选中）
+ * 3："一键登录"按钮点击
+ * code：0,1（0为协议勾选框未选中，1为选中）
+*/
+-(void)clShanYanActionListener:(NSInteger)type code:(NSInteger)code  message:(NSString *_Nullable)message;
 @end
 
 NS_ASSUME_NONNULL_BEGIN
 @interface CLShanYanSDKManager : NSObject
-
 
 /// 设置点击协议代理
 /// @param delegate 代理
@@ -45,9 +81,6 @@ NS_ASSUME_NONNULL_BEGIN
  @param complete 预初始化回调block 
  */
 +(void)initWithAppId:(NSString *)appId complete:(nullable CLComplete)complete;
-+(void)initWithAppId:(NSString *)appId
-              AppKey:(NSString *)appKey
-            complete:(nullable CLComplete)complete DEPRECATED_MSG_ATTRIBUTE("Method deprecated. Use `initWithAppId:complete:`");
 
 ///**
 // 设置初始化超时 单位:s
@@ -66,14 +99,15 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  * 预取号
  * 此调用将有助于提高闪验拉起授权页的速度和成功率
- * 建议在一键登录前提前调用此方法，比如调一键登录的vc的viewdidload中、初始化成功的回调中
+ * 建议在一键登录前提前调用此方法，比如调一键登录的vc的viewdidload中
  * 不建议在拉起授权页后调用
- * 回调中如需UI操作，建议自行切到主线程
+ * ⚠️‼️以 if (completeResult.error == nil) 为判断成功的依据，而非返回码
+ * ⚠️‼️此方法回调队列为dispatch_get_global_queue(0, 0)，回调中如需UI操作，请自行切到主线程
  */
 +(void)preGetPhonenumber:(nullable CLComplete)complete;
 
 /**
- * 一键登录
+ * 一键登录拉起内置授权页&获取Token
  @param clUIConfigure 闪验授权页参数配置
  @param complete 回调block
  * 回调中如需UI操作，建议自行切到主线程
@@ -83,11 +117,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 /**
- 一键登录 区分拉起授权页之前和之后的回调
+ 一键登录拉起内置授权页&获取Token( 区分拉起授权页之前和之后的回调)
  
  @param clUIConfigure 闪验授权页参数配置
  @param openLoginAuthListener 拉起授权页监听：拉起授权页面成功或失败的回调，拉起成功或失败均触发。当拉起失败时，oneKeyLoginListener不会触发。此回调的内部触发时机是viewDidAppear
-                    
+    
  @param oneKeyLoginListener 一键登录监听：拉起授权页成功后的后续操作回调，包括点击SDK内置的(非外部自定义)取消登录按钮，以及点击本机号码一键登录的回调。点击授权页自定义按钮不触发此回调
  
  * 回调中如需UI操作，建议自行切到主线程
@@ -99,31 +133,45 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  关闭授权页
- 注：若授权页未拉起，此方法调用无效果，complete不触发。内部实现为调用系统方法dismissViewcontroller:Complete。
+ 注：若授权页未拉起或已经提前关闭，此方法调用无效果，complete不触发。内部实现为调用系统方法dismissViewcontroller:Complete
  @param flag dismissViewcontroller`Animated, default is YES.
  @param completion dismissViewcontroller`completion
  */
 +(void)finishAuthControllerCompletion:(void(^_Nullable)(void))completion;
 +(void)finishAuthControllerAnimated:(BOOL)flag Completion:(void(^_Nullable)(void))completion;
 
+//设置checkBox勾选状态
++(void)setCheckBoxValue:(BOOL)isSelect;
 
-/**************本机认证功能***************/
-/**
- 本机号码校验
++(void)hideLoading;
 
- @param complete 校验回调
- */
+
+
+///**************一键登录获取Token***************/
+///// 注：此方法回调队列为dispatch_get_global_queue(0, 0)，如需UI操作请自行切入主线程
+//+(void)loginAuth:(CLComplete)complete;
+
+
+
+/**************本机认证(本机号码校验)***************/
 + (void)mobileCheckWithLocalPhoneNumberComplete:(CLComplete)complete;
 
-/**************本机认证功能***************/
 
 
+
+/**************SDK功能方法***************/
 /**
  模式控制台日志输出控制（默认关闭）
  @param enable 开关参数
  */
 + (void)printConsoleEnable:(BOOL)enable;
 
+
+/// 获取当前流量卡运营商，结果仅供参考
+// CTCC：电信、CMCC：移动、CUCC：联通、UNKNOW：未知
++ (NSString *)getOperatorType;
+
++ (void)clearScripCache;
 
 /**
  禁止日志上报(默认开启)
@@ -132,13 +180,14 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (void)forbiddenFullLogReport:(BOOL)forbidden;
 
++(void)sdkInit:(NSString *)appId complete:(nullable CLComplete)complete;
+
++ (BOOL)checkAuthEnable;
+
 /**
  * 当前SDK版本号
  */
 + (NSString *)clShanYanSDKVersion;
-
-+(void)sdkInit:(NSString *)appId complete:(nullable CLComplete)complete;
-
 @end
 
 NS_ASSUME_NONNULL_END
--- CLShanYan_Hbuild_demo/nativeplugins/CLSDK-ShanYanSDKModule/ios/OAuth.framework/Headers/ZOAuthManager.h
+++ CLShanYan_Hbuild_demo/nativeplugins/CLSDK-ShanYanSDKModule/ios/OAuth.framework/Headers/ZOAuthManager.h
@@ -5,21 +5,33 @@
 //  Created by zhangQY on 2019/5/13.
 //  Copyright © 2019 com.zzx.sdk.ios.test. All rights reserved.
 //
-//
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-@interface ZOAuthManager : NSObject
 
+
+@interface ZOAuthManager : NSObject
 /**
  *  是否使用测试环境
  *
  *  @param isDebug true／false
  */
-+ (void) setDebug:(Boolean) isDebug ;
++ (void) setDebug:(BOOL) isDebug ;
 
-//是否使用SHA256验签  默认NO
+/**
+ *  是否使用SHA256
+ *
+ *  @param schemeB true／false （默认是NO）
+ */
 + (void) useSchemeBInSecurityModule:(BOOL)schemeB;
 
+//设置UA
++ (void)setUAString:(NSString *)UAString;
+
+//删除保存在本地的UA
++ (void)removeUAString;
+
+//获取SDK版本信息
++ (NSString *)getVersionInfo;
 
 @end
